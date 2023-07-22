9a28dad9dfb5e2b645ce818561ff16cef84ab5b4
Mis-Tejaswini <129840367+Mis-Tejaswini@users.noreply.github.com>
2023-07-22 21:54:39 +0530

    Add files via upload

---
+++ front/src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+       <img src="img.jpg" class="card-img-top" alt="..."></img>
+    </div>
+  )
+}
+
+export default Home  
+
+// front end kru ka deploy bacnend first zal na backend ok kru thm
